a8b7c40049c35a95a322ec082b341204152b6add
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2024-12-29 00:47:12 +0900

    add EventHandling component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import ComponentProps from './components/ComponentProps.vue'
 import ComputedProperties from './components/ComputedProperties.vue'
+import EventHandling from './components/EventHandling.vue'
 import FormInputBindings from './components/FormInputBindings.vue'
 import Watchers from './components/Watchers.vue'
 </script>
 
 <template>
   <div class="container">
+    <EventHandling />
     <Watchers />
     <ComputedProperties />
     <ComponentProps stringProp="aaa" :numberProp="Number(100)" :booleanProp="true" :arrayProp="['aaa', 'bbb']"/>
---
+++ src/components/EventHandling.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, useTemplateRef } from 'vue'
+
+const count = ref(0)
+const input = useTemplateRef('input')
+
+const countUp = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <h1>Event Handling</h1>
+
+  <section>
+    <h2>Click</h2>
+    <button @click="countUp">Count Up</button>
+    <p>count: {{ count }}</p>
+  </section>
+
+  <section>
+    <h2>Key Modifier</h2>
+    <p>Pressing the Enter key removes the focus from the input field.</p>
+    <input ref="input" @keyup.enter="input?.blur" />
+  </section>
+</template>
